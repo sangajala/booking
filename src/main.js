a83d8ea4b83084c3ddefc9edb15f99acd6a277ae
sangajala <sriram.angajala@gmail.com>
2023-07-03 20:38:35 +0100

    fixed the image

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ export default function Main() {
   return (
     <main id="main">
       <div className="booking">
-        {/* <img src='https://mohammedthoufiq.simplybook.me/v2/themes/assets/img/categories/appearance.jpg' alt=''/> */}
+        <img src="appearance.jpg" alt="" style={{ width: 1000, height: 600 }} />
         <h1>Muhammathrafiq</h1>
         <button>
           Book Now <i className="fa fa-check fa-check"></i>
